9d29466a24dde4f33361d7af0f68b1b7abf50455
flniu <flniu@126.com>
2016-09-06 14:37:13 +0800

    support spaces in parameter

--- newpost.bat
+++ newpost.bat
@@ -3,6 +3,9 @@ if .%1==. (
     echo %0 post_file_name
     goto :EOF
 )
-set post_file_name=_posts\%DATE:~0,4%-%DATE:~5,2%-%DATE:~8,2%-%1.md
+set post_file_name=%*
+set post_file_name=%post_file_name:"=%
+set post_file_name=%post_file_name: =-%
+set post_file_name=_posts\%DATE:~0,4%-%DATE:~5,2%-%DATE:~8,2%-%post_file_name%.md
 echo newpost: %post_file_name%
 gvim.bat -c "0r $TEMPLATE/jekyll-post.md" %post_file_name%

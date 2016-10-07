9ac9849cf3e0fb12e6c1568f7919bc259358465c
flniu <flniu@126.com>
2016-10-07 11:43:56 +0800

    auto set date

--- _newpost.bat
+++ _newpost.bat
@@ -8,4 +8,4 @@ set post_file_name=%post_file_name:"=%
 set post_file_name=%post_file_name: =-%
 set post_file_name=_posts\%DATE:~0,4%-%DATE:~5,2%-%DATE:~8,2%-%post_file_name%.md
 echo newpost: %post_file_name%
-gvim.bat -c "0r $TEMPLATE/jekyll-post.md" %post_file_name%
+gvim.bat -c "0r $TEMPLATE/jekyll-post.md" -c "%%s/date: \zs/\=strftime('%%Y-%%m-%%d %%H:%%M:%%S')/" %post_file_name%
